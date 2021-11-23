7c609ef8ccafaef50608094e9399eca2859909d8
Caio Dias Cozendey <caiocozendey@hotmail.com>
2021-11-23 16:38:00 -0300

    COMMIT DIA VINTE E TRES

--- funcoes.php
+++ funcoes.php
@@ -14,6 +14,15 @@ function inicia($usuario, $senha){
         }
     }
 
+    function returnSize($dir){
+        return sizeof(array_values(array_diff(scandir($dir), array('.','..'))));
+    }
+
+    function returnDir(){
+        return 'C:\xampp\htdocs\Trabalho_Flavio_PHP_CaioDiasCozendey_GuilhermeAlbuquerqueFraga\Users';
+    }
+
+
     //Função para verificar se o saldo é positivo ou negativo após idade já validada
     function verificaSaldo($saldoDaFunction){
         if ($saldoDaFunction <= 0) {
@@ -22,6 +31,12 @@ function inicia($usuario, $senha){
             return false;
         }
     }
+
+    function num(){
+        return "3";
+    }
+
+    
 
     //Função que printa os valores após a confirmação da verifcação de saldo e idade
     //Encaminha pra tela conforme os resultados, utilizando o include
@@ -35,32 +50,26 @@ function inicia($usuario, $senha){
             if (verificaSaldo($saldofunction) == true) {
                 $print = 'Olá ' . ucfirst(trim($nomeFunction)) .'!<br> Seu saldo é de ' . str_replace(['R$', '-'], '',  $saldofunction) . ' negativos. <br> Não se desespere, nenhum obstáculo é tão grande para quem desiste!';
                 include('TelaSaldoNegativo.php');
-            } else {
-                $print = 'Olá ' . ucfirst(trim($nomeFunction)) .'! <br> Seu saldo é de ' . str_replace('R$', '', $saldofunction)  . '.<br> Não se alegre muito. A desgraça vem logo após a alegria.';
+            } 
+            else{
+                $print = 'Olá ' . ucfirst(trim($nomeFunction)) .'! <br> Seu saldo é de ' . str_replace('R$', '', $saldofunction) .  '.<br> Não se alegre muito. A desgraça vem logo após a alegria.';
                 include('TelaSaldoPositivo.php');
             }
         }
     }
     
     //Abre os 4 arquivos
-    $arq1 = fopen("./Users/user1.txt", "r");
-    $arq2 = fopen("./Users/user2.txt", "r");
-    $arq3 = fopen("./Users/user3.txt", "r");
-    $arq4 = fopen("./Users/user4.txt", "r");
+    $arq1 = fopen("./Users/1.txt", "r");
+    // $arq2 = fopen("./Users/2.txt", "r");
+    // $arq3 = fopen("./Users/3.txt", "r");
+    // $arq4 = fopen("./Users/4.txt", "r");
 
-    //Divide todas as linhas do arquivo em vetores para melhor manipulação
-    //TESTE - TESTE - TESTE - TESTE - TESTE - TESTE - TESTE - TESTE - TESTE - TESTE - TESTE - TESTE - TESTE - TESTE
-    // $dir = "./Users/";
-    // $Users = scandir($dir);
-    // $tamanho = sizeof($Users);
-
-    // for ($i = 1; $i < $tamanho; $i++) { 
-    //     $user[$i] = file(".Users.user[$i].txt");
-    // }
-    $user1 = file("./Users/user1.txt");
-    $user2 = file("./Users/user2.txt");
-    $user3 = file("./Users/user3.txt");
-    $user4 = file("./Users/user4.txt");
+    // //Divide todas as linhas do arquivo em vetores para melhor manipulação
+    
+    // $user1 = file("./Users/1.txt");
+    // $user2 = file("./Users/2.txt");
+    // $user3 = file("./Users/3.txt");
+    // $user4 = file("./Users/4.txt");
 
     //Vericia os valores que o usuário inseriu no html passado como parametro e defini os valores das variaveis
     //de acordo com o usuário selecionado
@@ -70,39 +79,46 @@ function inicia($usuario, $senha){
     //Se der ponto extra humilde 
 
     //Define o fuso horário de São Paulo
-    // for ($i = 0; $i   < ; $i + +) { 
-    //     # code...
-    // }
-
+    
     date_default_timezone_set('America/Sao_Paulo');
-    if ($usuario == trim($user1[0]) && $senha == trim($user1[1])) {
-        $nome = $user1[0];
-        $dataNascimento = $user1[2];
-        $saldo = $user1[3];
-        login($nome, $dataNascimento, $saldo);
-    } elseif ($usuario == trim($user2[0]) && $senha == trim($user2[1])) {
-        $nome = $user2[0];
-        $dataNascimento = $user2[2];
-        $saldo = $user2[3];
-        login($nome, $dataNascimento, $saldo);
-    } elseif ($usuario == trim($user3[0]) && $senha == trim($user3[1])) {
-        $nome = $user3[0];
-        $dataNascimento = $user3[2];
-        $saldo = $user3[3];
-        login($nome, $dataNascimento, $saldo);
-    } elseif ($usuario == trim($user4[0]) && $senha == trim($user4[1])) {
-        $nome = $user4[0];
-        $dataNascimento = $user4[2];
-        $saldo = $user4[3];
-        login($nome, $dataNascimento, $saldo);
-    } else {
-        echo "<script> alert('Usuário inválido!')</script>";
-        include('index.php');
+    $local_dir = returnDir();
+    $tamanhoArq = returnSize($local_dir);
+    for ($i = 0, $x = 1; $i < $tamanhoArq, $x < $tamanhoArq+1; $i++, $x++) { 
+        $useropen = fopen("./Users/".$x.".txt");
+        $user = file("./Users/".$x.".txt");
+        if ($usuario == trim($user[$x[0]]) && $senha == trim($user[$x[1]])) {
+            // $nome = $user[$i][0];
+            // $dataNascimento = $user[$i][2];
+            // $saldo = $user[$i][3];
+            // login($nome, $dataNascimento, $saldo);
+            // break;
+            echo $user[$i[1]];  
+        }
+        else {
+            echo "<script> alert('Usuário inválido!')</script>";
+            include('index.php');
+            break;
+        }
     }
 
-    //Fecha os arquivos
-    fclose($arq1);
-    fclose($arq2);
-    fclose($arq3);
-    fclose($arq4);
+    // if ($usuario == trim($user1[0]) && $senha == trim($user1[1])) {
+    //     $nome = $user1[0];
+    //     $dataNascimento = $user1[2];
+    //     $saldo = $user1[3];
+    //     login($nome, $dataNascimento, $saldo);
+    // } elseif ($usuario == trim($user2[0]) && $senha == trim($user2[1])) {
+    //     $nome = $user2[0];
+    //     $dataNascimento = $user2[2];
+    //     $saldo = $user2[3];
+    //     login($nome, $dataNascimento, $saldo);
+    // } elseif ($usuario == trim($user3[0]) && $senha == trim($user3[1])) {
+    //     $nome = $user3[0];
+    //     $dataNascimento = $user3[2];
+    //     $saldo = $user3[3];
+    //     login($nome, $dataNascimento, $saldo);
+    // } elseif ($usuario == trim($user4[0]) && $senha == trim($user4[1])) {
+    //     $nome = $user4[0];
+    //     $dataNascimento = $user4[2];
+    //     $saldo = $user4[3];
+    //     login($nome, $dataNascimento, $saldo);
 }
--- register.php
+++ register.php
@@ -9,6 +9,12 @@
     $saldo_html = $_REQUEST["saldo"];
     $dataNasciment_html = $_REQUEST["dataNascimento"];
 
+    function returnSize($dir){
+        return sizeof(array_values(array_diff(scandir($dir), array('.','..'))));
+    }
+    $local_dir = 'C:\xampp\htdocs\Trabalho_Flavio_PHP_CaioDiasCozendey_GuilhermeAlbuquerqueFraga\Users';
+    $sizefiles = returnSize($local_dir);
+
     /* Função que verifica se as senha são iguais */
     function verificaSenha($senha1, $senha2){
         if ($senha1 == $senha2) {
@@ -21,10 +27,13 @@
 
     /* Verifica o retorno da função verificaSenha e executa a função de registro */
     if (verificaSenha($password1_html, $password2_html) == true){
-        registro($user_html, $password1_html, $saldo_html, $dataNasciment_html);
+        for ($i = 0, $x = 1; $i < 3, $x < 3; $i++, $x++) { 
+            echo "caralho$i";   
+        }
+        //verificaRegistro($user_html, $password1_html, $saldo_html, $dataNasciment_html);
     }
     else{
-        echo "<script> alert('A primeira senha não está igual a segunda'); </script>";
+        echo "<script> alert('As senha não são iguais!'); </script>";
         include('registerscreen.php');
     }
 

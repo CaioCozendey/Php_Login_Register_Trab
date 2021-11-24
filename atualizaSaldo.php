d90e9a97fe42207202eb462f37753455915eea37
Caio Dias Cozendey <caiocozendey@hotmail.com>
2021-11-24 16:41:34 -0300

    pagina saldo

--- atualizaSaldo.php
+++ atualizaSaldo.php
@@ -15,13 +15,14 @@
             <h1 class=""> Kappa Barretina </h1>
             <div class="img-circular"></div>
             <!-- Formulário com usuario, senha e botão de login -->
-            <div>
+            <p class="frase"> Caso o novo saldo seja 0, digite o número "0" </p>
+            <input type="number" name="saldo" placeholder="Digite seu novo saldo">
+            <div class="center">
                 <form method="post">
-                    <input>
-                    <div class="center">
-                    <a href="atualizaSaldo.php"> <button class="buttonSaldo" type="submit"> Novo Saldo </button></a>
-                    <a href="index.php"><button class="buttonSaldo" type="submit"> Sair </button></a>
-                    </div>  
+                        <button class="buttonSaldo" type="submit"> Novo Saldo </button>
+                </form>
+                <form method="post" action="index.php">
+                    <button class="buttonSaldo" type="submit"> Sair </button>
                 </form>
             </div>
         </div>
